5412fa19323037a210653ed54fa784bba7610d9f
iivann404 <123344320+iivann404@users.noreply.github.com>
2026-01-31 17:30:28 -0600

    Archivo de extraccion de caracteres de una subcadena

---
+++ extraccion_subcadenas_substring.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e "\nStep 4: Substring Extraction"
+
+STRING="The quick brown fox jumps over the lazy dog"
+START=10
+LENGTH=5
+
+SUBSTRING=${STRING:$START:$LENGTH}
+
+echo "The original string is: $STRING"
+echo "Extracting 5 characters starting from position 10:"
+echo "The substring is: $SUBSTRING"
